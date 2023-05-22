0e8c645a6f92342fc8c82e03c7efb66a14d4f121
Neela Kanta <neelakantareddy.padigepati@gmail.com>
2023-05-22 19:08:32 +0530

    refactored code

--- components/shortCell.php
+++ components/shortCell.php
@@ -10,15 +10,22 @@ $shortLink ="http://".$_SERVER['HTTP_HOST'].parse_url($_SERVER["REQUEST_URI"], P
             </form>
         </div>
         <div class="control">
-            <button class="button button-primary button-block button-shadow" form="submit-link" type="submit">Make Anonymous</button>
+            <button class="button button-primary button-block button-shadow" onclick="clearLink()" >Short Another Link</button>
         </div>
 </div>
+
+<script>
+    function clearLink() {
+
+        window.location.href = window.location.href.split('?')[0];
+    }
+</script>
 
 <?php $qrURL = 'https://chart.apis.google.com/chart?cht=qr&chs=300x300&chl='.$shortLink; ?>
 <div class="container newsletter-form is-revealing mt-3 ">
 <div class="btn-group mb-3" role="group" aria-label="Basic example">
-  <button type="button" class="button button-primary button-shadow" onclick="copyIt()">Copy</button>
-  <button type="button" class="button btn-primary button-shadow" onclick="downloadQR('<?= $qrURL ?>')">Download</button>
+  <button type="button" class="button button-primary button-shadow" onclick="copyIt()">Copy Link</button>
+  <button type="button" class="button btn-primary button-shadow" onclick="downloadQR('<?= $qrURL ?>')">Download QR</button>
 </div>
 <center><img style="width:250px;" class="qrimg" src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=<?php echo $shortLink; ?>"></center>
 </div>
